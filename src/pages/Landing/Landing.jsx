8d31d7e78d05c6cab62fbfe5a670b32fd0ffb722
Edoardo Pessina <edoardopessina.priv@gmail.com>
2021-03-21 23:43:31 +0100

    Started landing bg

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,6 +6,11 @@ import Hero from '../../components/Hero/Hero'
 export default function Landing () {
   return (
     <header className={styles.header}>
+      <div className={styles.bg_container}>
+        <ul className={styles.bg_shapes}>
+          <li />
+        </ul>
+      </div>
       <Hero />
     </header>
   )
